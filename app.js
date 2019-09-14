bbdfa35ada7f9ee7df5ab9761b78a16bf12ddc89
ChristopherJack <cj138@student.london.ac.uk>
2019-09-14 22:59:48 +0700

    add magic and style

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const NEWSAPIKEY = '6d3bf7bff5324831a84530cf4ea8f757';
-const SEARCHURL = 'https://newsapi.org/v2/everything';
-
+const NEWSAPIKEY = "6d3bf7bff5324831a84530cf4ea8f757";
+const SEARCHURL = "https://newsapi.org/v2/everything";
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
@@ -28,46 +27,45 @@ function checkStatus(response) {
 }
 
 function generateResults(data) {
-    $("#top-headlines").empty();
+  $("#top-headlines").empty();
   for (let i = 0; i < data.length; i++) {
-    let content = [`${data[i].content}`]
-    let newId = 'read-me' + i
-     $("#top-headlines").append(
-      `<div>
-        <li>
-            <h3><a href=${data[i].url} target="_blank">${data[i].title}</a></h3>
+    let content = [`${data[i].content}`];
+    let newId = "read-me" + i;
+    $("#top-headlines").append(
+      `<section class="card">
+          <img src="${data[i].urlToImage}"/>
+            <h1 class="card-title"><a href=${data[i].url} target="_blank">${data[i].title}</a></h1>
             <p><span>${data[i].source.name}</span></p>
             <p>${data[i].description}</p>
             <p id='${newId}' hidden="true">${content}</p>
-           <button onclick="responsiveVoice.speak(document.getElementById('${newId}').textContent);" 
-            type="button" value="Play">Read English to Me!</button>
-        </li>
-      </div>`
+<div>
+           <button class="read-to-me" onclick="responsiveVoice.speak(document.getElementById('${newId}').textContent);" 
+            type="button">Read English to Me!</button>
+            </div>
+      </section>`
     );
   }
 }
 
-function newSearch(searchTerm = 'technology'){
-    const params ={
-        q: searchTerm,
-        apiKey: NEWSAPIKEY,
-    };
+function newSearch(searchTerm = "technology") {
+  const params = {
+    q: searchTerm,
+    apiKey: NEWSAPIKEY
+  };
 
-    let queryString = $.param(params);
-    const url = SEARCHURL + "?" + queryString;
+  let queryString = $.param(params);
+  const url = SEARCHURL + "?" + queryString;
 
-    fetchData(url).then(data => generateResults(data.articles));
+  fetchData(url).then(data => generateResults(data.articles));
 }
 
-function formSearch(){
-    $('form').submit(event =>{
-        event.preventDefault();
-        const searchTerm = $("#search-term").val();
-        newSearch(searchTerm);
-        $('#search-term').val("");
-    })
-    
+function formSearch() {
+  $("form").submit(event => {
+    event.preventDefault();
+    const searchTerm = $("#search-term").val();
+    newSearch(searchTerm);
+    $("#search-term").val("");
+  });
 }
 
 $(formSearch);
-
